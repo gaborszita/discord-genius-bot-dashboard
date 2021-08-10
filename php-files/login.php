bd31b41fd5539cec0f666f93868f59176cd6ebe3
Gabor Szita <gaborszita88@gmail.com>
2021-08-10 15:40:51 -0700

    Add GNU GPLv3 license

--- php-files/login.php
+++ php-files/login.php
@@ -1,4 +1,25 @@
 <?php
+/*
+ * Copyright 2020-2021 Gabor Szita
+ *
+ * This file is part of Discord Genius Bot.
+ *
+ * Discord Genius Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Discord Genius Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Discord Genius Bot.  If not, see <https://www.gnu.org/licenses/>.
+ */
+?>
+
+<?php
 ini_set('max_execution_time', 300); //300 seconds = 5 minutes. In case if your CURL is slow and is loading too much (Can be IPv6 problem)
 
 error_reporting(E_ALL);
